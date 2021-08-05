41653ceb4e21d9d2e33acfe38c9ce26c7d030f87
Snu <83713969+Snu22@users.noreply.github.com>
2021-08-05 20:36:32 -0300

    install packges for self bot

---
+++ install.sh
@@ -0,0 +1,9 @@
+{
+  python3 -m pip install discord
+  python3 -m pip install requests
+  python3 -m pip install colorama
+} || {
+  python -m pip install discord
+  python -m pip install requests
+  python -m pip install colorama
+}

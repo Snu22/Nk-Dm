55561ab62089d582da3db69be66f7539d512782b
Snu <83713969+Snu22@users.noreply.github.com>
2021-08-05 20:45:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
-{
-  clear
-  python3 -m pip install discord
-  python3 -m pip install requests
-  python3 -m pip install colorama
-} || {
-  clear
-  python -m pip install discord
-  python -m pip install requests
-  python -m pip install colorama
-}
+clear
+python3 -m pip install discord || clear && python -m pip install discord
+python3 -m pip install requests || clear && python -m pip install requests
+python3 -m pip install colorama || clear && python -m pip install colorama

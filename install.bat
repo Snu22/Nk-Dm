2e1afd0ca9511ca7bf67a989fec098cc9cc7f1b8
Snu <83713969+Snu22@users.noreply.github.com>
2021-08-05 20:54:34 -0300

    install.bat
    install packges for windows

---
+++ install.bat
@@ -0,0 +1,11 @@
+python -m pip install discord
+cls
+py -m pip install discord
+cls
+python -m pip install requests
+cls
+py -m pip install requests
+cls
+python -m pip install colorama
+cls
+py -m pip install colorama
